1815c7b0882012fcea8ca95739adfa29ef4b4830
Aftab Ali <aftabali28882@gmail.com>
2020-05-31 13:02:27 +0500

    This is yaml update
    this is yamal update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todo from './components/Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'meeting with boss',
+        completed: true
+      }
+    ]
+  }
+
+  markComplete =(id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todo todos={this.state.todos} markComplete={this.markComplete}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const myelement = (
+    <table>
+      <tr>
+        <th>Name</th> 
+      </tr>
+      <tr>
+        <td>John</td>
+      </tr>
+      <tr>
+        <td>Elsa</td>
+      </tr>
+    </table>
+  );
+class Car extends React.Component {
+    render() {
+      return <h2>I am a {this.props.color} Car!</h2>;
+    }
+}
+  
+  ReactDOM.render(<Car color="red"/>, document.getElementById('roo'));
+  ReactDOM.render(<App/>, document.getElementById('app'));
+  
+  ReactDOM.render(myelement, document.getElementById('root'));
+  ReactDOM.render(<p>Hallo</p>, document.getElementById('sandy'));
